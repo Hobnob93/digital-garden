741584f2d57d8725ee20a944c69b2c6f34d9c9a1
Kyle H <kyle.hobdey@hotmail.co.uk>
2026-02-14 20:35:57 +0000

    Completed ContentCardLink component + links in JourneyLogs page

--- src/DigitalGarden/DigitalGarden.Shared.Components/Controls/ContentCardLink.razor.cs
+++ src/DigitalGarden/DigitalGarden.Shared.Components/Controls/ContentCardLink.razor.cs
@@ -1,9 +1,22 @@
+using Microsoft.AspNetCore.Components;
 using Microsoft.AspNetCore.Components.Web;
 
 namespace DigitalGarden.Shared.Components.Controls;
 
 public partial class ContentCardLink
 {
+    [Parameter, EditorRequired]
+    public string Name { get; set; } = string.Empty;
+
+    [Parameter, EditorRequired]
+    public string Description { get; set; } = string.Empty;
+
+    [Parameter, EditorRequired]
+    public string FontAwesomeIcon { get; set; } = string.Empty;
+
+    [Parameter, EditorRequired]
+    public EventCallback ClickedCallback { get; set; }
+
     private bool IsPulsing { get; set; }
     private bool IsFlipped { get; set; }
 
@@ -35,5 +48,10 @@ public partial class ContentCardLink
     {
         IsPulsing = false;
         await InvokeAsync(StateHasChanged);
+    }
+
+    private async Task OnClicked(MouseEventArgs e)
+    {
+        await ClickedCallback.InvokeAsync();
     }
 }
--- src/DigitalGarden/DigitalGarden/DigitalGarden.Client/Components/Pages/JourneyLogs.razor.cs
+++ src/DigitalGarden/DigitalGarden/DigitalGarden.Client/Components/Pages/JourneyLogs.razor.cs
@@ -1,6 +1,24 @@
+using Microsoft.AspNetCore.Components;
+
 namespace DigitalGarden.Client.Components.Pages;
 
 public partial class JourneyLogs
 {
+    [Inject]
+    private NavigationManager Navigation { get; set; } = default!;
 
+    private void GoToNow()
+    {
+        Navigation.NavigateTo("/journey-logs/now");
+    }
+
+    private void GoToProfessionalProfile()
+    {
+        Navigation.NavigateTo("/journey-logs/mission-log");
+    }
+
+    private void GoToWedding()
+    {
+        Navigation.NavigateTo("/journey-logs/09-09-2025");
+    }
 }
