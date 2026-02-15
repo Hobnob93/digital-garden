7af408b78a7ca8fbae1c486b15a472aabefae61b
Kyle H <kyle.hobdey@hotmail.co.uk>
2026-02-15 15:00:12 +0000

    Renamed Journey Logs to Life Log page

---
+++ src/DigitalGarden/DigitalGarden/DigitalGarden.Client/Components/Pages/LifeLog.razor.cs
@@ -0,0 +1,24 @@
+using Microsoft.AspNetCore.Components;
+
+namespace DigitalGarden.Client.Components.Pages;
+
+public partial class LifeLog
+{
+    [Inject]
+    private NavigationManager Navigation { get; set; } = default!;
+
+    private void GoToProfessionalProfile()
+    {
+        Navigation.NavigateTo("/life-log/mission");
+    }
+
+    private void GoToWedding()
+    {
+        Navigation.NavigateTo("/life-log/09-09-2025");
+    }
+
+    private void GoToHolidays()
+    {
+        Navigation.NavigateTo("/life-log/journeys");
+    }
+}
